d80c27313b8882ee5b6e868221965fdbf293800a
M Pranay <pranaymusaramthota@gmail.com>
2025-03-12 01:18:18 +0530

    add backend-common package
    Can access the jwt secret from the common package which contains config.ts

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express();
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 app.post('/signup', (req, res)=>{
     const {username, password} = req.body;
     
---
+++ packages/backend-common/src/config.ts
@@ -0,0 +1,1 @@
+export const JWT_SECRET = process.env.JWT_SECRET || "876765eBRUNO(U%#Y%$WX&G^";
